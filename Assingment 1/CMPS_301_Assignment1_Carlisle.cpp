2412e6ed4ca9d61479f75ce1b6b7a5cfdb13a33c
gage-carlisle <gage.carlisle@laverne.edu>
2025-10-17 13:41:27 -0700

    Assignment 1

---
+++ Assingment 1/CMPS_301_Assignment1_Carlisle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    // Header
+    cout << "===============================================" << endl;
+    cout << "Name: Gage Carlisle" << endl;
+    cout << "Fall 2025 Class Schedule" << endl;
+    cout << "CMPS 301: Programming Concepts - Fall 2025" << endl;
+    cout << "===============================================" << endl << endl;
+
+    // Table Header
+    cout << "Day\t\tTime\t\tCourse Code\tCourse Title\t\t\tLocation\t\tInstructor" << endl;
+    cout << "--------------------------------------------------------------------------------------------------------------------" << endl;
+
+    // Class Entries
+    cout << "Tue/Thurs\t10:20 AM - 11:55 AM\tCMPS 301\tProgramming Concepts\tFH 207\t\tProf. Rodriguez" << endl;
+    cout << "Tue\t\t7:50 PM - 10:00 PM\tCMPS 218\tPublishing on the Web 1\tFH 207\t\tProf. Goetz" << endl;
+    cout << "Mon\t\t5:00 PM - 8:15 PM\tBUS 105\t\tBusiness Writing\tOnline\t\tProf. Coleman" << endl;
+    cout << "Wed\t\t5:30 PM - 8:30 PM\tECON 150\tEconomics\t\tOnline\t\tProf. Troxel" << endl;
+    cout << "Fri\t\t10:00 AM - 12:45 PM\tHIST 210\tWorld History\t\tLH 203\t\tProf. Johnson" << endl;
+    cout << "Mon/Wed\t\t8:00 AM - 12:15 PM\tCSCI 220\tData Structures\t\tFH 207\t\tProf. Rodriguez" << endl;
+
+    // Total Units
+    cout << "\nTotal Units: 18" << endl;
+
+    // Message
+    cout << "\nLet's crush Fall 2025! Stay focused and code on!" << endl;
+
+    return 0;
+}
